392164db8472b7c2d24e5440cbdff92319331851
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-14 16:25:51 -0700

    Moved all html files into the project root folder

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"], // Looks for files in the root
   theme: {
     extend: {},
   },
